ebab38aa0b1baff19177cf58d7f6a09a6d6d9ea3
Jye Harry <jyeharry21@hotmail.com>
2023-08-13 21:00:22 +1000

    Start theme

---
+++ src/lib/theme.ts
@@ -0,0 +1,7 @@
+const theme = {
+  breakpoints: {
+    sm: '375px',
+    md: '768px',
+    lg: '1440px',
+  },
+}
--- src/space-tourism-website/components/Navbar/index.tsx
+++ src/space-tourism-website/components/Navbar/index.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image"
+import logo from 'public/space-tourism-website/logo.svg'
+
 const Navbar = () => {
   return (
     <nav>
+      <Image
+        src={logo}
+        alt='logo'
+      />
+      <div>line</div>
       <ul>
         <li>00 Home</li>
         <li>01 Destination</li>
